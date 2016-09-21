4fa7c1a9ead69f2bc9647f51547644d3be83c3bf
Kristina McElveen <kmcelveen.sp26@gmail.com>
2016-09-20 21:05:03 -0700

    solved some

--- app/arrays.js
+++ app/arrays.js
@@ -2,19 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if(arr[i] === item){
+        return i;
+      }
+    };
+    return -1;
   },
 
   sum: function(arr) {
-
+    var results = 0;
+    arr.forEach( function(element, index) {
+      results += element;
+    });
+    return results;
   },
 
   remove: function(arr, item) {
-
+    return arr.filter(function(element){return element !== item});
   },
-
+// [ 1, 2, 3, 4, 2 ]
   removeWithoutCopy: function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++) {
+       if(arr[i] === item){
+          arr.splice(i, 2);
+       }
+     }
+     return arr;
   },
 
   append: function(arr, item) {
